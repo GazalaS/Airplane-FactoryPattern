d63197b86fc8f5df5779192d3ebd0a7a50c1846e
Gazala Sayyad <gazalafshaikh@gmail.com>
2022-06-12 17:13:02 +0000

    WIP: MockFileSystem for Test

--- Airplane.UnitTests/AirplaneNavigationServiceTests.cs
+++ Airplane.UnitTests/AirplaneNavigationServiceTests.cs
@@ -3,6 +3,7 @@ using Airplane.Models;
 using Airplane.Services;
 using Microsoft.Extensions.DependencyInjection;
 using Moq;
+using System.IO.Abstractions.TestingHelpers;
 
 namespace Airplane.UnitTests
 {
@@ -27,9 +28,56 @@ namespace Airplane.UnitTests
             Assert.DoesNotThrow(Action);
         }
 
+        [TestCase]
+        public void WhenMockFile_ValidInput_Then_Navigate()
+        {
+            var mockFileSystem = new MockFileSystem();
+            var mockInputFile = new MockFileData("forward 4\nup 2\ndive 3");
+            var fileName = @"C:\temp\valid.txt";
+            mockFileSystem.AddFile(fileName, mockInputFile);
+
+            //arrange
+            var mockMovementFactory = new Mock<IMovementFactory>().Object;
+            var mockReadInputService = new Mock<IReadInputService>().Object;
+            var mockPosition = new Mock<Position>().Object;
+            var mockWriteOutputService = new Mock<IWriteOutputService>().Object;
+
+            var airplaneServiceNavigation = new AirplaneNavigationService(mockMovementFactory, mockReadInputService, mockWriteOutputService);
+
+            //act
+            void Action() => airplaneServiceNavigation.Navigate(fileName);
+
+            //assert
+            Assert.DoesNotThrow(Action);
+        }
+
         [TestCase(@"C:\Input\commandsUnknown.txt")]
         public void When_InvalidInput_Then_ThrowException(string fileName)
         {
+            //arrange
+            var serviceProvider = ServicesExtensions.BuildServiceProvider();
+            var movementFactory = serviceProvider.GetService<IMovementFactory>();
+            var readInputService = serviceProvider.GetService<IReadInputService>();
+            var writeOutputService = serviceProvider.GetService<IWriteOutputService>();
+
+            var airplaneServiceNavigation = new AirplaneNavigationService(movementFactory, readInputService, writeOutputService);
+
+            //act
+            void Action() => airplaneServiceNavigation.Navigate(fileName);
+
+            //assert
+            var exception = Assert.Throws<InvalidDataException>(Action);
+            Assert.That(exception.Message, Is.EqualTo("Invalid input."));
+        }
+
+        [TestCase]
+        public void WhenMockFile_InvalidInput_Then_ThrowException()
+        {
+            var mockFileSystem = new MockFileSystem();
+            var mockInputFile = new MockFileData("forward 4\nup 2\nunknown 3");
+            var fileName = @"C:\temp\invalid.txt";
+            mockFileSystem.AddFile(fileName, mockInputFile);
+
             //arrange
             var serviceProvider = ServicesExtensions.BuildServiceProvider();
             var movementFactory = serviceProvider.GetService<IMovementFactory>();
